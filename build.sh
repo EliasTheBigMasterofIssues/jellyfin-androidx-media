79b53ab97fcd83107626574cf8adfd3678bcc47f
EliasTheBigMasterofIssues <eliascontato@protonmail.com>
2026-06-01 09:20:08 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,9 @@ export ANDROID_NDK_PATH=$ANDROID_HOME/ndk/26.1.10909125
 # Setup environment
 export ANDROIDX_MEDIA_ROOT="${PWD}/media"
 export FFMPEG_MOD_PATH="${ANDROIDX_MEDIA_ROOT}/libraries/decoder_ffmpeg/src/main"
+export MPEGH_MOD_PATH="${ANDROIDX_MEDIA_ROOT}/libraries/decoder_mpegh/src/main"
 export FFMPEG_PATH="${PWD}/ffmpeg"
-export ENABLED_DECODERS=(flac alac pcm_mulaw pcm_alaw mp3 aac ac3 eac3 dca mlp truehd iamf mpegh)
+export ENABLED_DECODERS=(flac alac pcm_mulaw pcm_alaw mp3 aac ac3 eac3 dca mlp truehd)
 
 # Create softlink to ffmpeg
 ln -sf "${FFMPEG_PATH}" "${FFMPEG_MOD_PATH}/jni/ffmpeg"
@@ -17,3 +18,8 @@ ln -sf "${FFMPEG_PATH}" "${FFMPEG_MOD_PATH}/jni/ffmpeg"
 # Start build
 cd "${FFMPEG_MOD_PATH}/jni"
 ./build_ffmpeg.sh "${FFMPEG_MOD_PATH}" "${ANDROID_NDK_PATH}" "linux-x86_64" 23 "${ENABLED_DECODERS[@]}"
+
+git clone https://github.com/Fraunhofer-IIS/mpeghdec.git "${MPEGH_MOD_PATH}/jni" --branch r3.0.2 --depth=1 libmpegh
+cd "${MPEGH_MOD_PATH}/jni"
+cmake -S libmpegh -B . -G Ninja
+cmake --build .
